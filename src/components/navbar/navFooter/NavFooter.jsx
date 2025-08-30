89ee4316ee08ff3d90239bae6b122c0bdfd31f6e
samsul alam <samsul7718@gmail.com>
2025-08-30 23:10:08 +0530

    nav footer completed

--- src/components/navbar/navFooter/NavFooter.jsx
+++ src/components/navbar/navFooter/NavFooter.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import {Link,useLocation} from 'react-router-dom'
 import { navbarData } from '../../../data/navbarData'
+import { FiMenu } from 'react-icons/fi';
 
 const NavFooter = () => {
     const location=useLocation();
   return (
     <section className='w-full bg-gray-100 h-14 flex items-center justify-between font-bold my-2  px-4 shadow-md'>
-        {navbarData.map((data,index)=>(
+         {/* only for "All" */}
+        <div className="flex items-center gap-1 cursor-pointer hover:text-blue-600">
+          <FiMenu size={18} />
+          <Link
+            to="/"
+            className={`transition ${
+              location.pathname === "/" ? "text-blue-600 font-semibold" : "text-gray-700"
+            }`}
+          >
+            All
+          </Link>
+        </div>
+        {navbarData
+        .filter((item) => item.name !== "All") 
+        .map((data,index)=>
+          index <= 9 ? (
             <Link 
              key={index}
             to={data.path}
@@ -15,8 +31,10 @@ const NavFooter = () => {
           }`}
             >
             {data.name}
+            
             </Link>
-        ))}
+        )
+        : null)}
     </section>
   )
 }
