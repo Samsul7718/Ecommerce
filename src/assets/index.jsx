93c50178e0e012140cbc92bb8cb33252202a6b3b
samsul alam <samsul7718@gmail.com>
2025-09-07 21:18:16 +0530

    working on slider

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,7 +1,8 @@
 import logo from '../../src/assets/images/s-logo.png';
 import hero from '../../src/assets/images/heroimg2.jpeg';
-
-
+import hero1 from '../../src/assets/images/heroimg3.webp'
+import hero2 from '../../src/assets/images/heroimg4.png'
+import hero3 from '../../src/assets/images/heroimg5.jpg'
 
 
 
@@ -10,4 +11,7 @@ import hero from '../../src/assets/images/heroimg2.jpeg';
 export{
     logo,
     hero,
+    hero1,
+    hero2,
+    hero3,
 }
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { hero,hero1,hero2 } from '../../assets';
+import { hero,hero1,hero2,hero3 } from '../../assets';
 
-const images=[hero,hero1,hero2]
+const images=[hero,hero1,hero2,hero3]
 const Hero = () => {
   const [currentIndex, setCurrentIndex] =useState(0);
 
@@ -22,7 +22,7 @@ const Hero = () => {
         <div
           className="absolute inset-0 bg-cover bg-center"
         >
-            <img src={hero} alt="" />
+            <img src={images[currentIndex]} alt=""  className="w-full h-full object-cover" />
         </div>
           {/* Gradient fade (image -> skyblue) */}
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-sky-200"></div>
@@ -38,14 +38,14 @@ const Hero = () => {
         {/* Left Arrow */}
       <button 
       onClick={prevSlide}
-      className="absolute left-4 top-1/2 -translate-y-25 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-0">
+      className="absolute left-4 top-1/2 -translate-y-25 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
         <ChevronLeft className="text-black" size={24} />
       </button>
 
       {/* Right Arrow */}
       <button
       onClick={nextSlide}
-      className="absolute right-4 top-1/2 -translate-y-25 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-0">
+      className="absolute right-4 top-1/2 -translate-y-25 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
         <ChevronRight className="text-black" size={24} />
       </button>
    </section>
