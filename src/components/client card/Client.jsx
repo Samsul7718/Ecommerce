ad94aa4a4fa090c6136c8c2704ada607a3c541cd
samsul alam <samsul7718@gmail.com>
2025-09-12 00:02:14 +0530

    button created

--- src/components/client card/Client.jsx
+++ src/components/client card/Client.jsx
@@ -3,9 +3,11 @@ import ClientCard from './ClientCard'
 
 const Client = () => {
   return (
-    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 pt-6
+    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-6
       bg-sky-100'>
         
+       <ClientCard/>
+       <ClientCard/>
        <ClientCard/>
        <ClientCard/>
        <ClientCard/>
--- src/components/client card/ClientCard.jsx
+++ src/components/client card/ClientCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { hero3 } from '../../assets'
 
-const ClientCard = () => {
+const ClientCard = ({title,subTitle}) => {
   return (
     <section className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden">
         <div className="p-4 text-center">
             <h2 className="text-xl md:text-2xl font-semibold text-gray-800">What Our Clients Say</h2>
             <p className="text-gray-600 mt-2 md:text-lg">Testimonials from our satisfied customers</p>
         </div>
-        <div className='p-4'>
-            <img src={hero3} alt=""  className="w-full h-48 object-cover rounded-md"/>
+        <div className='p-2 grid grid-cols-2 gap-6 '>
+            <div>
+                <img src={hero3} alt=""  className="w-full h-24 object-cover rounded-md"/>
+                  <p className="text-gray-600 mt-2 md:text-xs mx-8">Testimonials</p>
+            </div>
+            
+            <img src={hero3} alt=""  className="w-full h-24 object-cover rounded-md"/>
+            <img src={hero3} alt=""  className="w-full h-24 object-cover rounded-md"/>
+            <img src={hero3} alt=""  className="w-full h-24 object-cover rounded-md"/>
         </div>
-        <div className='p-4 text-center'>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+        <div className='p-4 text-center space-between flex gap-20 justify-center'>
+            <button className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-700 hover:text-white
+             px-1 py-1">
+                See More...
+            </button>
+            <button className="bg-green-500 text-black px-2 py-2 rounded hover:bg-green-700 hover:text-white
+             px-1 py-1">
                 Add to Cart
             </button>
         </div>
