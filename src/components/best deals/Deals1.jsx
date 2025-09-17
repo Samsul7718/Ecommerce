c9f79d7ce8a252ae89ede46004f6a029ce6c7fec
samsul alam <samsul7718@gmail.com>
2025-09-17 19:46:53 +0530

    modyfing deals1 card

--- src/components/best deals/Deals1.jsx
+++ src/components/best deals/Deals1.jsx
@@ -4,7 +4,7 @@ import { shirt,pant,T_shirt,formal } from '../../assets'
 
 const Deals1 = () => {
   return (
-    <section id ='102' className='w-full my-16 px-4'>
+    <section id ='102' className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-full my-16 px-4'>
         <Deals_Card
          title={"Mens Fashion"}
                subTitle={"Discover the latest trends in"}
@@ -46,6 +46,26 @@ const Deals1 = () => {
                ]}
         />
         <Deals_Card
+         title={"Mens Fashion"}
+               subTitle={"Discover the latest trends in"}
+               items={[
+                 {id:1, title:"Shirts", src: shirt},
+                 {id:2, title:"Pants", src: pant},
+                 {id:3, title:"T-shirt", src: T_shirt},
+                 {id:4, title:"Formals", src: formal},
+               ]}
+        />
+           <Deals_Card
+         title={"Mens Fashion"}
+               subTitle={"Discover the latest trends in"}
+               items={[
+                 {id:1, title:"Shirts", src: shirt},
+                 {id:2, title:"Pants", src: pant},
+                 {id:3, title:"T-shirt", src: T_shirt},
+                 {id:4, title:"Formals", src: formal},
+               ]}
+        />
+           <Deals_Card
          title={"Mens Fashion"}
                subTitle={"Discover the latest trends in"}
                items={[
