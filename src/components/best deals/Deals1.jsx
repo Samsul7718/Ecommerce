cf9426a46c37b25cb1e85b9be91f0810280d3504
samsul alam <samsul7718@gmail.com>
2025-09-17 21:53:47 +0530

    data modyfing

--- src/components/best deals/Deals1.jsx
+++ src/components/best deals/Deals1.jsx
@@ -1,80 +1,49 @@
-import React from 'react'
-import Deals_Card from './Deals_Card'
+import React, { useState } from 'react'
+// import Deals_Card from './Deals_Card'
 import { shirt,pant,T_shirt,formal } from '../../assets'
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
+const images=[shirt,pant,T_shirt,formal]
 const Deals1 = () => {
+  const [currentIndex, setCurrentIndex] =useState(0);
+
+  const prevSlide=()=>{
+      setCurrentIndex((prevIndex)=>
+        prevIndex===0? images.length-1:prevIndex-1
+      );
+  }
+   const nextSlide=()=>{
+      setCurrentIndex((prevIndex)=>
+        currentIndex=== images.length-1? 0:prevIndex+1
+      );
+  }
   return (
-    <section id ='102' className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-full my-16 px-4'>
-        <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-        <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-        <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-        <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-        <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-           <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
-           <Deals_Card
-         title={"Mens Fashion"}
-               subTitle={"Discover the latest trends in"}
-               items={[
-                 {id:1, title:"Shirts", src: shirt},
-                 {id:2, title:"Pants", src: pant},
-                 {id:3, title:"T-shirt", src: T_shirt},
-                 {id:4, title:"Formals", src: formal},
-               ]}
-        />
+    <section id ='102' className='relative flex flex-col w-full my-16 px-4 bg-sky-100'>
+        <div className='absolute inset-0'>
+      <div className="p-4 text-center">
+            <h2 className="text-xl md:text-2xl font-semibold text-gray-800">best deal for the week</h2>
+            <p className="text-gray-600 mt-2 md:text-lg">choice is yours</p>
+        </div>
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+        >
+            <img src={images[currentIndex]} alt=""  className="w-full h-full object-cover" />
+        </div>
+       </div>
+
+          {/* Left Arrow */}
+      <button 
+      onClick={prevSlide}
+      className="absolute left-4 top-1/2 -translate-y-2 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
+        <ChevronLeft className="text-black" size={26} />
+      </button>
+
+      {/* Right Arrow */}
+      <button
+      onClick={nextSlide}
+      className="absolute right-4 top-1/2 -translate-y-2 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
+        <ChevronRight className="text-black" size={26} />
+      </button>
 
     </section>
   )
--- src/components/best deals/Deals_Card.jsx
+++ src/components/best deals/Deals_Card.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { hero3 } from '../../assets'
+// import React from 'react'
+// import { hero3,hero4,hero2 } from '../../assets'
 
-const Deals_Card = ({title,subTitle,items}) => {
-  return (
-    <section className='max-w-sm mx-auto bg-white'>
-        <div className='p-4 text-center'>
-            <h2 className='text-xl md:text-2xl font-semibold text-gray-800'>{title}</h2>
-            <p className='text-gray-600 mt-2 text-gray-200'>{subTitle}</p>
-        </div>
-        <div className='grid grid-cols-2 gap-3 p-4'>
-            {items.map((item)=>(
+// const Deals_Card = ({title,subTitle,items}) => {
+//   return (
+//     <section className='max-w-sm mx-auto bg-white'>
+//         <div className='p-4 text-center'>
+//             <h2 className='text-xl md:text-2xl font-semibold text-gray-800'>{title}</h2>
+//             <p className='text-gray-600 mt-2 text-gray-200'>{subTitle}</p>
+//         </div>
+//         <div className='grid grid-cols-2 gap-3 p-4'>
+//             {items.map((item)=>(
 
-            <div id={item.id} className=''>
-                <img src={item.src} alt="" className='w-full h-24 object-cover'/>
-                <p>{item.title}</p>
-            </div>
-          ))}
-        </div>
-        <div className='p-4 text-center space-between flex gap-20 justify-center'>
-            <button className=''>See More...</button>
-        </div>
+//             <div id={item.id} className=''>
+//                 <img src={item.src} alt="" className='w-full h-24 object-cover'/>
+//                 <p>{item.title}</p>
+//             </div>
+//           ))}
+//         </div>
+//         <div className='p-4 text-center space-between flex gap-20 justify-center'>
+//             <button className=''>See More...</button>
+//         </div>
 
-    </section>
-  )
-}
+//     </section>
+//   )
+// }
 
-export default Deals_Card
+// export default Deals_Card
