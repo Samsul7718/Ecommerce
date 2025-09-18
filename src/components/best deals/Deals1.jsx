16e6de99fa5d58f41c4744ee2d9ad9300c0653c5
samsul alam <samsul7718@gmail.com>
2025-09-18 22:22:53 +0530

    adding data

--- src/components/best deals/Deals1.jsx
+++ src/components/best deals/Deals1.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 // import Deals_Card from './Deals_Card'
 import { shirt,pant,T_shirt,formal} from '../../assets'
+import { burkha,saree,kurti,long,cheast} from '../../assets'
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
-const images=[shirt,pant,T_shirt,formal]
+const images=[shirt,pant,T_shirt,formal,burkha,saree,kurti,long,cheast]
 const Deals1 = () => {
   const [currentIndex, setCurrentIndex] =useState(0);
 
@@ -18,18 +19,33 @@ const Deals1 = () => {
       );
   }
   return (
-    <section id ='102' className='relative w-full my-16 px-4 bg-sky-100'>
+    <section id ='102' className='relative w-full px-4 bg-sky-100'>
        
-       <div className="p-4 text-center">
+       <div className=" relative p-4 text-center">
             <h2 className="text-xl md:text-2xl font-semibold text-gray-800">best deal for the week</h2>
             <p className="text-gray-600 mt-2 md:text-lg">choice is yours</p>
         </div>
+
+         <div className="flex gap-4 overflow-x-auto scrollbar-hide p-4">
+    {images[currentIndex].map((img, index) => (
+      <div
+        key={index}
+        className="min-w-[200px] flex-shrink-0 rounded-lg overflow-hidden shadow-md"
+      >
+        <img
+          src={img}
+          alt=""
+          className="w-full h-40 object-cover"
+        />
+      </div>
+    ))}
+  </div>
          {/* <div className='absolute inset-0'> */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
+        {/* <div
+          className="relative grid grid-cols-5 gap-4 overflow-x-auto p-4"
         >
-            <img src={images[currentIndex]} alt=""  className="w-full h-full object-cover" />
-        </div>
+            <img src={images[currentIndex]} alt=""  className="w-40px h-full object-cover" />
+        </div> */}
        {/* </div> */}
 
           {/* Left Arrow */}
