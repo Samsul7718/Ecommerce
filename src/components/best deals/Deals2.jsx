ce60b00ddaa1d053eae5adb3c50a72b1dbea4999
samsul alam <samsul7718@gmail.com>
2025-09-23 00:03:08 +0530

    added images

--- src/components/best deals/Deals2.jsx
+++ src/components/best deals/Deals2.jsx
@@ -4,7 +4,7 @@ const Deals2 = () => {
   return (
     <section id='' className='relative w-full px-4 bg-sky-100'>
          <div className=" relative p-4 text-start">
-            <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Great Deal | get upto 45% off on essential kitchen</h2>
+            <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Great Deal | Get upto 45% off on essential kitchen</h2>
             {/* <p className="text-gray-600 mt-2 md:text-lg">choice is yours come fast</p> */}
         </div>
     </section>
