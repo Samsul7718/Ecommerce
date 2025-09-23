cad208a4e4f90a358ab82dc3e02af082bcf92727
samsul alam <samsul7718@gmail.com>
2025-09-23 20:14:20 +0530

    deals2 completed

--- src/components/best deals/Deals1.jsx
+++ src/components/best deals/Deals1.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, {useRef } from 'react'
 // import Deals_Card from './Deals_Card'
 import { shirt,pant,T_shirt,formal, dumble, starx} from '../../assets'
 import { burkha,saree,kurti,long,cheast} from '../../assets'
@@ -7,32 +7,32 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 const images=[shirt,pant,T_shirt,formal,burkha,saree,kurti,long,cheast,dumble,starx]
 const Deals1 = () => {
   const containerRef = useRef(null);
-  // const [currentIndex, setCurrentIndex] =useState(0);
+  
 
   const scrollLeft=()=>{
     if(containerRef.current){
-      const imgWidth = containerRef.current.firstChild.offsetWidth + 16; // width + gap
-      containerRef.current.scrollBy({ left: -imgWidth, behavior: "smooth" });
+      const containerWidth = containerRef.current.offsetWidth; // width + gap
+      containerRef.current.scrollBy({ left: -containerWidth, behavior: "smooth" });
     }
   }
    const scrollRight=()=>{
     if(containerRef.current){
-     const imgWidth = containerRef.current.firstChild.offsetWidth + 16;
-      containerRef.current.scrollBy({ left: imgWidth, behavior: "smooth" });
+     const containerWidth = containerRef.current.offsetWidth;
+      containerRef.current.scrollBy({ left:containerWidth, behavior: "smooth" });
     }
   }
   return (
     <section id ='102' className='relative w-full px-4 bg-sky-100'>
        
        <div className=" relative p-4 text-start">
-            <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Best deal for this week | Up to 35% off on Women fashion</h2>
-            {/* <p className="text-gray-600 mt-2 md:text-lg">choice is yours come fast</p> */}
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Best deal for this week | Up to 35% off on fashion</h2>
+    
         </div>
 
          <div 
           ref={containerRef}
           className="flex gap-4 overflow-x-hidden scroll-smooth p-4"
-          style={{ scrollSnapType: "x mandatory" }}
+          style={{ scrollSnapAlign: "start" }}
          >
     {images.map((img, index) => (
       <div
@@ -47,24 +47,15 @@ const Deals1 = () => {
       </div>
     ))}
   </div>
-         {/* <div className='absolute inset-0'> */}
-        {/* <div
-          className="relative grid grid-cols-5 gap-4 overflow-x-auto p-4"
-        >
-            <img src={images[currentIndex]} alt=""  className="w-40px h-full object-cover" />
-        </div> */}
-       {/* </div> */}
-
-          {/* Left Arrow */}
+     
       <button 
       onClick={scrollLeft}
       className="absolute left-4 top-3/5 -translate-y-2 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
         <ChevronLeft className="text-black" size={24} />
       </button>
 
-      {/* Right Arrow */}
       <button
-      onClick={scrollRight}
+      onClick={scrollRight} 
       className="absolute right-4 top-3/5 -translate-y-2 bg-white/60 hover:bg-white p-3 rounded-full shadow-md z-20">
         <ChevronRight className="text-black" size={24} />
       </button>
--- src/components/best deals/Deals2.jsx
+++ src/components/best deals/Deals2.jsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { ac_container,bowl,chaicup,ciger,coco_chopper,container,dalpress,glass,ice_maker,
+  karai,kutni,rotibelun,spray,thali,toast_turn
+ } from '../../assets'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 
+ const images=[ac_container,bowl,chaicup,ciger,coco_chopper,container,dalpress,glass,ice_maker,
+  karai,kutni,rotibelun,spray,thali,toast_turn]
 const Deals2 = () => {
+  const containerRef = useRef(null);
+
+  const scrollLeft=()=>{
+    if(containerRef.current){
+      const containerWidth=containerRef.current.offsetWidth; // width + gap
+      containerRef.current.scrollBy({left:-containerWidth, behavior:"smooth"})
+    }
+  }
+   const scrollRight=()=>{
+      if(containerRef.current){
+      const containerWidth=containerRef.current.offsetWidth;
+      containerRef.current.scrollBy({left:containerWidth, behavior:"smooth"})
+    }
+  }
   return (
     <section id='' className='relative w-full px-4 bg-sky-100'>
          <div className=" relative p-4 text-start">
-            <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Great Deal | Get upto 45% off on essential kitchen</h2>
-            {/* <p className="text-gray-600 mt-2 md:text-lg">choice is yours come fast</p> */}
+          <h2 className="text-xl md:text-2xl font-semibold text-gray-800">Great Deal | Get upto 45% off on essential kitchen</h2>
         </div>
-        <div>
-            
+        <div 
+        ref={containerRef}
+        className='flex gap-4 overflow-x-auto scroll-smooth p-4'
+        style={{scrollSnapAlign:"start"}}
+        >
+          {images.map((img,index)=>(
+            <div key={index}
+            className='min-w-[200px] shrink-0 rounded-lg overflow-hidden shadow-md'
+            >
+             <img
+             src={img}
+             alt="kitchenimg"
+             className='w-full h-40 object-cover'
+             />
+            </div>
+          ))}
         </div>
+
+        <button
+        onClick={scrollLeft}
+        className='absolute left-4 top-3/6 -translate-y-2 bg-white/60 rounded-full p-3 hover:bg-white shadow-md z-20'>
+        <ChevronLeft className="text-black" size={24} />
+        </button>
+
+        <button
+        onClick={scrollRight}
+        className='absolute right-4 top-3/6 -translate-y-2 bg-white/60 rounded-full p-3 hover:bg-white shadow-md z-20'>
+        <ChevronRight className="text-black" size={24} />
+        </button>
+
     </section>
   )
 }
