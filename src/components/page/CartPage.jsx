0dec75458a120104d2aa05c47a473593032cce25
samsul alam <samsul7718@gmail.com>
2025-09-27 23:04:12 +0530

    wrapping app by cartprovider

--- src/components/page/CartPage.jsx
+++ src/components/page/CartPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CartPage = () => {
   return (
     <div className=''>
-        <h1>Your Shopping Cart</h1>
+        <h1 className='text-2xl font-bold mb-5'>Your Shopping Cart</h1>
         <div className=''>
             <div
             key=""
