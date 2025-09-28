59db20d83d3904b419f73e1a8e389f7ca6f62c37
samsul alam <samsul7718@gmail.com>
2025-09-28 22:34:31 +0530

    modyfying cart page

--- src/components/page/CartPage.jsx
+++ src/components/page/CartPage.jsx
@@ -4,23 +4,23 @@ const CartPage = () => {
   return (
     <div className='max-w-5xl mx-auto p-5'>
         <h1 className='text-2xl font-bold mb-5'>Your Shopping Cart</h1>
-        <div className=''>
+        <div className='space-y-5'>
             <div
             key=""
-            className=''>
-                <div>
+            className='flex items-center justify-between border p-4 rounded-lg shadow-sm'>
+                <div className='flex items-center gap-4'>
                       <img 
                         src="" 
                         alt="" 
-                        className=''
+                        className='w-20 h-20 object-cover rounded-lg'
                         />
                     <div>
                       
-                        <h1 className=''>name</h1>
-                        <p className=''>price</p>
+                        <h1 className='font semi-bold'>name</h1>
+                        <p className='text-gray-500'>price</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex items-center gap-3'>
                     <button>-</button>
                     <span>qty</span>
                     <button>+</button>
