c615361486ba1baf068001a4eefa13cc48364638
samsul alam <samsul7718@gmail.com>
2025-10-10 15:59:23 +0530

    vanity product created

---
+++ src/products/vanity/Vanity.jsx
@@ -0,0 +1,45 @@
+import {iphone17,Oneplus,samg,oppo} from "../../assets";
+
+const Vanity = [
+ 
+  {
+    id: "002",
+    name: "Iphone🍎",
+    price: 1299.99,
+    description: "new iphone series launch ",
+    images: [iphone17,Oneplus,samg,oppo], 
+    colors: ["black","orange", "blue"],
+    size: ["15cm", "17cm"],
+    
+  },
+  {
+    id: "003",
+    name: "Oneplus✚",
+    price: 1099.99,
+    description: "New oneplus great features and camera",
+    images: [Oneplus,samg,oppo,iphone17],
+    colors: ["black","red", "blue"],
+    size: ["15cm", "17cm"],
+    
+  },
+   {
+    id: "004",
+    name: "Samgsung",
+    price: 99.99,
+    description: "New samsung series with new idea and features",
+    images: [samg,oppo,iphone17,Oneplus],
+    colors: ["black","red", "blue"],
+    size: ["15cm", "17cm"],
+  
+  },
+   {
+    id: "005",
+    name: "Oppo",
+    price: 699.99,
+    description: "New process with camera and features",
+    images: [oppo,iphone17,Oneplus,samg],
+    colors: ["black","red", "blue"],
+    size: ["15cm", "17cm"],
+  },
+];
+export {Vanity};
