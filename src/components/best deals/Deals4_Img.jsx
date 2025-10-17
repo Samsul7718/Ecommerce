5d4407a77c0dbb4629e02516967061003d5eed98
samsul alam <samsul7718@gmail.com>
2025-10-17 10:49:29 +0530

    import all chairs images in deals4_image

---
+++ src/components/best deals/Deals4_Img.jsx
@@ -0,0 +1,22 @@
+import { chair1,chair2,chair3,chair4,chair5,chair6,chair7,chair8,chair9,chair10 } from "../../assets";
+import { chair11,chair12,chair13,chair14,chair15 } from "../../assets";
+
+const images=[
+    {src:chair1,link:"/chair"},
+    {src:chair2,link:"/chair"},
+    {src:chair3,link:"/chair"},
+    {src:chair4,link:"/chair"},
+    {src:chair5,link:"/chair"},
+    {src:chair6,link:"/chair"},
+    {src:chair7,link:"/chair"},
+    {src:chair8,link:"/chair"},
+    {src:chair9,link:"/chair"},
+    {src:chair10,link:"/chair"},
+    {src:chair11,link:"/chair"},
+    {src:chair12,link:"/chair"},
+    {src:chair13,link:"/chair"},
+    {src:chair14,link:"/chair"},
+    {src:chair15,link:"/chair"},
+
+]
+export {images};
