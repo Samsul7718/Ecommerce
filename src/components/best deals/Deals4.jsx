3508e3f89045e72de3ca4ff20294f1ef8471091e
samsul alam <samsul7718@gmail.com>
2025-10-19 19:44:15 +0530

    working on left and right scroll function

--- src/components/best deals/Deals4.jsx
+++ src/components/best deals/Deals4.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import {images} from '../best deals/Deals4_Img'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 const Deals4 = () => {
+  const containerRef=useRef(null);
+  const scrollLeft=()=>{
+      if(containerRef.current){
+        const containerWidth=containerRef.current.ofsetWidth;
+       containerRef.current.scrollBy({left:-containerWidth,behavior:"smooth"})
+      }
+  }
+  const scrollRight=()=>{
+    if(containerRef.current){
+      const containerWidth=containerRef.current.ofsetWidth;
+      containerRef.current.scrollBy({right:containerWidth,behavior:"smooth"})
+    }
+  }
   return (
     <section className='relative w-full px-4 bg-sky-100'>
       <div>
         <h2 className='font-semibold text-xl md:text-2xl text-gray-800'>Great deals on sesons Festivals | Get upto 55% off on office chair</h2>
       </div>
-      <div className='flex overflow-x-hidden scroll-smooth p-4'>
+      <div
+      ref={containerRef}
+      className='flex overflow-x-hidden scroll-smooth p-4'>
         {images.map((img,index)=>(
           <div
           key={index}
@@ -23,11 +38,13 @@ const Deals4 = () => {
         
       </div>
       <button
+      onClick={scrollLeft}
       className='absolute left-4 top-3/6 -translate-y-2 bg-white/60 rounded-full p-3 shadow-md hover:bg-cyan-200'
       >
         <ChevronLeft className="text-black" size={24} />
       </button>
       <button
+      onClick={scrollRight}
       className='absolute right-4 top-3/6 -translate-y-2 bg-white/60 rounded-full p-3 shadow-md hover:bg-cyan-200' 
       >
         <ChevronRight className="text-black" size={24} />
