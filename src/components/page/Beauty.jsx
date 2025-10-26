375d988021c978b4cb3403d7f236c99fbf9f7018
samsul alam <samsul7718@gmail.com>
2025-10-26 18:36:54 +0530

    created beauty page

--- src/components/page/Beauty.jsx
+++ src/components/page/Beauty.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Beauty = () => {
   return (
-    <div>Beauty</div>
+    <section>
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Beauty Products</h1>
+        <p className="text-gray-700">
+          Discover our range of beauty products designed to enhance your natural glow. From skincare essentials to makeup must-haves, we have everything you need to look and feel your best.
+        </p>
+      </div>
+    </section>
   )
 }
 
