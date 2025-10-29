c1fb1f312d9726cc936cc3637e02a9803666d70b
samsul alam <samsul7718@gmail.com>
2025-10-29 10:01:57 +0530

    working on order page

--- src/components/page/OrderPage.jsx
+++ src/components/page/OrderPage.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const OrderPage = () => {
   return (
-    <div>OrderPage</div>
+    <div className='flex flex-col items-center mt-10'>
+        <h2 className='font-bold text-gray-700 text-xl'>Order Successful!</h2>
+          <div>
+                <p className='text-center mt-10'>Thank you for your purchase. Your order has been placed and is being processed.</p>
+                <div className='flex justify-center mt-5'>
+                  
+                    <button className='bg-green-500 text-white font-semibold px-6 py-2 rounded-full hover:bg-green-600 transition'>
+                        order sucessful
+                    </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
