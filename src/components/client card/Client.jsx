c71bcadd22d162733a800223aced8c8e81a9d77a
samsul alam <samsul7718@gmail.com>
2025-11-26 11:44:15 +0530

    products url added from backend

--- src/components/client card/Client.jsx
+++ src/components/client card/Client.jsx
@@ -8,7 +8,18 @@ import { Toys } from '../../products/toy/Toy'
 import { Shoes } from '../../products/shoe/Shoe'
 import { Electronic } from '../../products/electronics/Electronic'
 import { Beauty } from '../../products/beauty/Beauty'
+import { useState } from 'react'
+import { useEffect } from 'react'
 const Client = () => {
+  const [products,setProducts]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/products')
+    .then(res=>res.json())
+    .then(data=>setProducts(data))
+    .catch(err=>console.log(err));
+  },[])
+  console.log("product from backend",products);
   return (
     <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-6 p-4
       bg-sky-100'>
@@ -16,50 +27,50 @@ const Client = () => {
        <ClientCard
        title={"Mens Fashion"}
        subTitle={"Discover the latest trends in"}
-       items={MenProducts}
+       items={products}
        seeMorePage={"/menfashion"}
       
        />
         <ClientCard
        title={"Womens Fashion"}
        subTitle={"Discover the latest trends in"}
-       items={GirlProducts}
+       items={products}
         seeMorePage={"/womenfashion"}
        />
         <ClientCard
        title={"Gym"}
        subTitle={"Get into your fitness world"}
-       items={GymProducts}
+       items={products}
         seeMorePage={"/gymfashion"}
        />
         <ClientCard
        title={"Toy's"}
        subTitle={"Get relaxed with toy's"}
-       items={Toys}
+       items={products}
        seeMorePage={"/KidsToy"}
        />
         <ClientCard
        title={"Kitchen Appliances"}
        subTitle={"Introduce the best kitchen appliances"}
-       items={KitchProducts}
+       items={products}
        seeMorePage={"/kittools"}
        />
         <ClientCard
        title={"Shoes"}
        subTitle={"Step style with latest shoe collection"}
-       items={Shoes}
+       items={products}
        seeMorePage={"/Shoes"}
        />
         <ClientCard
        title={"Best of Electronics"}
        subTitle={"Explore top-rated electronics"}
-       items={Electronic}
+       items={products}
        seeMorePage={"/electronic"}
        />
         <ClientCard
        title={"Best seller in Beauty"}
        subTitle={"Discover the latest trends in"}
-       items={Beauty}
+       items={products}
        seeMorePage={"/Beauty"}
        />
 
