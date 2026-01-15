6bce46aee9851b17e42107a6a314f0652fb2fd94
samsul alam <samsul7718@gmail.com>
2026-01-15 14:26:24 +0530

    working on client 2 page

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { use } from 'react'
 // import ClientCard2 from './ClientCard2'
 // import { Laptop } from '../../products/laptop/Laptop'
 // import { Mobile } from '../../products/mobile/Mobile'
@@ -6,15 +6,17 @@ import React from 'react'
 // import { Electronic1 } from '../../products/electronic1/Electronic1'
 
 const Client2 = () => {
+  const [prosucts,setProducts]=useState([]);
+  
   return (
     
     <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 bg-sky-100 pt-6 p-4'>
-         {/* <ClientCard2
+         <ClientCard2
          title={"Laptop"}
          subTitle={"Introducing new arrival series"}
          items={Laptop}
          viewPage={'/laptop'}
-         /> */}
+         />
          {/* <ClientCard2
          title={"Mobile"}
          subTitle={"Introducing new arrival series"}
