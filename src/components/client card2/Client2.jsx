45da1fe0a138f61ecb735c62520e226dc9cc47fd
samsul alam <samsul7718@gmail.com>
2026-02-07 14:58:36 +0530

    mobile sub title updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -25,7 +25,7 @@ const Client2 = () => {
          />
          <ClientCard2
          title={"Mobile"}
-         subTitle={"Explore the newest mobile phones with advanced features"}
+         subTitle={"Explore the newest mobile phones with advanced feature"}
          items={products.filter(p=>p.category==="mobile")}
          viewPage={'/mobile'}
          />
