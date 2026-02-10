324974aca26770ba7e6388a97e149cd1e358865a
samsul alam <samsul7718@gmail.com>
2026-02-10 19:34:53 +0530

    vanity sub title updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -31,7 +31,7 @@ const Client2 = () => {
          />
          <ClientCard2
          title={"Vanity"}
-         subTitle={"Discover our collection of stylish vanity product"}
+         subTitle={"Discover our collection of stylish vanity products"}
          items={products.filter(p=>p.category==="vanity")}
          viewPage={'/vanity'}
          />
