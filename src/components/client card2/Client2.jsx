ea1ef84cb3e78abf52c4b90e33a502ff7e077413
samsul alam <samsul7718@gmail.com>
2026-02-15 22:41:40 +0530

    updated product system and sub title

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -37,7 +37,7 @@ const Client2 = () => {
          />
          <ClientCard2
          title={"Electronic_daily"}
-         subTitle={"Introducing new electronic daily essential  product for your need"}
+         subTitle={"Introducing new electronic daily essential  product for your needs"}
          items={products.filter(p=>p.category==="electronic")}
          viewPage={'/electronic'}
          />
