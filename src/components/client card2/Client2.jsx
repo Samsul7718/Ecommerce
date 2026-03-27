5248b23b31356f7fbda93ff04f242f6a852b269d
samsul alam <samsul7718@gmail.com>
2026-03-27 14:36:05 +0530

    furniture product updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -55,7 +55,7 @@ const Client2 = () => {
          />
            <ClientCard2
          title={"Furniture"}
-         subTitle={"New designed  daily essential furniture products for your daily need"}
+         subTitle={"New designed  daily essential furniture products for your daily needs"}
          items={Electronic1}
          viewPage={'/furniture'}
          />
