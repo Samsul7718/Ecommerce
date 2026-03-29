a74afb94e58a2b96fee2572fdbfddfcd45f1d7f2
samsul alam <samsul7718@gmail.com>
2026-03-29 22:09:01 +0530

    juwelry product updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -61,7 +61,7 @@ const Client2 = () => {
          />
             <ClientCard2
          title={"Juwelry"}
-         subTitle={"Introducing daily essential  newly arrived juwelry products for your daily need"}
+         subTitle={"Introducing daily essential  newly arrived juwelry products for your"}
          items={Electronic1}
          viewPage={'/juwelry'}
          />
