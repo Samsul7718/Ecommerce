cf3ac59c2d904ef60d678914e94a7d9ccb4c03f5
samsul alam <samsul7718@gmail.com>
2026-04-09 20:48:26 +0530

    mobile product updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -25,7 +25,7 @@ const Client2 = () => {
          />
          <ClientCard2
          title={"Mobile"}
-         subTitle={"Explore the newest mobile phones with cutting-edge features design connectivity"}
+         subTitle={"Explore the newest mobile phones with cutting-edge feature design connectivity"}
          items={products.filter(p=>p.category==="mobile")}
          viewPage={'/mobile'}
          />
