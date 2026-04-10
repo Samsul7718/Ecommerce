75ed0eaf1ef1a8a3ffe840bf9f25e54ed7ddfcbd
samsul alam <samsul7718@gmail.com>
2026-04-10 19:40:27 +0530

    electronics product updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -37,7 +37,7 @@ const Client2 = () => {
          />
          <ClientCard2
          title={"Electronic_daily"}
-         subTitle={"Introducing newly arrived electronic products for your daily essential needs"}
+         subTitle={"Introducing newly arrived electronic products for your daily essential need"}
          items={products.filter(p=>p.category==="electronic")}
          viewPage={'/electronic'}
          />
