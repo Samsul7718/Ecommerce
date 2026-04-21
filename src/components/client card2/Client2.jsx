a1d164e7d5a7038152a95490ac8b76435bfbf6a1
samsul alam <samsul7718@gmail.com>
2026-04-21 22:14:06 +0530

    grocery product page updated

--- src/components/client card2/Client2.jsx
+++ src/components/client card2/Client2.jsx
@@ -43,7 +43,7 @@ const Client2 = () => {
          />
           <ClientCard2
          title={"Grocery"}
-         subTitle={"Discover fresh and high-quality grocery products for your daily need"}
+         subTitle={"Discover fresh and high-quality grocery products for your daily needs"}
          items={Electronic}
          viewPage={'/grocery'}
          />
