75f522ffd978711b712bea475f875e70d086cb2b
samsul alam <samsul7718@gmail.com>
2026-05-16 13:33:08 +0530

    beauty product page title updated

--- src/components/page/Beauty.jsx
+++ src/components/page/Beauty.jsx
@@ -6,7 +6,7 @@ const Beauty = () => {
       <div>
         <h1 className="text-3xl font-bold mb-4">Beauty Products</h1>
         <p className="text-gray-700">
-          Discover our range of beauty products designed to enhance your natural glow. From skincare essentials to makeup must-haves, we have everything you need to look and feel your best.
+          Discover our range of beauty products designed to enhance your natural glow. From skincare essential to makeup must-haves, we have everything you need to look and feel your best.
         </p>
         <button className='bg-green-500 p-4 rounded-lg shadow-md'>add to cart</button>
       </div>
